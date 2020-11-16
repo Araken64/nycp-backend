b09198ee8e591f8714db25ba09fe220b52323a21
Araken64 <elian.araken@gmail.com>
2020-11-16 17:45:42 +0100

    Basic model

--- src/models/CriminalCase.ts
+++ src/models/CriminalCase.ts
@@ -1,1 +1,7 @@
-import mongoose from 'mongoose';
+import * as Mongoose from 'mongoose';
+
+const criminalCaseSchema: Mongoose.Schema = new Mongoose.Schema({
+  criminalCaseNumber: { type: String, required: true },
+});
+
+export default Mongoose.model('CriminalCase', criminalCaseSchema);
