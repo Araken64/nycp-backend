9247cd42cb1ae7fe4c560bb139ef8f87ee76ec57
TheTaoueille <alan.rousic@orange.fr>
2020-11-16 19:53:09 +0100

    Modification Prisoner.ts

--- src/models/Prisoner.ts
+++ src/models/Prisoner.ts
@@ -2,20 +2,22 @@ import * as Mongoose from 'mongoose';
 import CriminalCase from './CriminalCase';
 
 const prisonerSchema: Mongoose.Schema = new Mongoose.Schema({
-  prisonFileNumber: { type: String, maxlength: 10, index: true },
+  prisonFileNumber: {
+    type: String, maxlength: 10, index: true, unique: true,
+  },
   givenName: { type: String, maxlength: 30 },
   surname: { type: String, maxlength: 30 },
-  dateOfBirth: { type: Date },
+  dateOfBirth: { type: Date, max: Date.now() },
   placeOfBirth: { type: String, maxlength: 30 },
   dateOfIncaceration: { type: Date },
   motiveLabel: { type: String, maxlength: 50, required: true },
-  juridictionName: { type: String, maxlength: 30, require: true },
+  juridictionName: { type: String, maxlength: 30, required: true },
   criminalCase: { type: [CriminalCase], required: true },
   decision:
   {
     type: [
       {
-        type: { type: String, maxlength: 1, require: true },
+        type: { type: String, maxlength: 1, required: true },
         dateOfDecision: { type: Date },
         duration: { type: Number },
         dateOfFinalDischarge: { type: Date },
