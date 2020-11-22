404befcd55130a9fee1ce78fba98010680b82740
Araken64 <elian.araken@gmail.com>
2020-11-22 22:08:34 +0100

    add PUT and DELETE tests for criminal cases

--- src/__test__/criminalcases.test.ts
+++ src/__test__/criminalcases.test.ts
@@ -4,13 +4,13 @@ import app from '../app';
 import CriminalCaseModel from '../models/CriminalCase';
 
 describe('Testing the criminal cases API', () => {
-  it('tests the base route and returns true for status', async () => {
+  it('tests the GET all criminal cases route', async () => {
     const res = await supertest(app).get('/api/criminalcases');
 
     expect(res.status).toBe(200);
   });
 
-  it('tests the post new criminal case endpoint and returns as success message', async () => {
+  it('tests the POST new criminal cases route', async () => {
     const response = await supertest(app).post('/api/criminalcases').send({
       criminalCaseNumber: 'fakeNumber',
       juridictionName: 'fakeJuridiction',
@@ -22,13 +22,25 @@ describe('Testing the criminal cases API', () => {
     expect(response.body.message).toBe('Object saved !');
   });
 
-  afterEach(async () => {
+  it('tests the PUT modified prisoner route', async () => {
+    const res = await supertest(app).put('/api/criminalcases/fakeNumber').send({
+      givenName: 'fakeGivenName2',
+    });
+
+    expect(res.status).toBe(200);
+    expect(res.body.message).toBe('Object modified !');
+  });
+
+  it('tests the DELETE prisoner route', async () => {
+    const res = await supertest(app).delete('/api/criminalcases/fakeNumber');
+    expect(res.status).toBe(200);
+    expect(res.body.message).toBe('Object deleted !');
+  });
+
+  afterAll(async () => {
     await CriminalCaseModel.deleteOne({
       criminalCaseNumber: 'fakeNumber',
     });
-  });
-
-  afterAll(() => {
     mongoose.disconnect();
   });
 });
