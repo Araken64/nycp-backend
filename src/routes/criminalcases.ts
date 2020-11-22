346fe1ed0eaf164c7c639072cfd1646caccff763
Araken64 <elian.araken@gmail.com>
2020-11-22 22:10:02 +0100

    add PUT and DELETE route for criminal Case

--- src/routes/criminalcases.ts
+++ src/routes/criminalcases.ts
@@ -5,5 +5,7 @@ const router = express.Router();
 
 router.post('/', casesCtrl.createCriminalCase);
 router.get('/', casesCtrl.getAllCriminalCases);
+router.put('/:criminalCaseNumber', casesCtrl.modifyCriminalCase);
+router.delete('/:criminalCaseNumber', casesCtrl.deleteCriminalCase);
 
 export default router;
