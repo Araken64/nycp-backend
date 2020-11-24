4530d4ed2761b0692d6352cdf755842a058055fc
Araken64 <elian.araken@gmail.com>
2020-11-24 23:31:27 +0100

    change name to actions.ts

--- src/routes/preventive.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { prevention } from '../controllers/preventive';
-
-const router = express.Router();
-
-router.put('/:prisonFileNumber&:criminalCaseNumber', prevention);
-
-export default router;
