b984f573623745e4bf0f6c368ea77467203cb085
Araken64 <elian.araken@gmail.com>
2020-12-14 16:24:55 +0100

    resolve functions call

--- src/routes/actions.ts
+++ src/routes/actions.ts
@@ -1,12 +1,14 @@
 import express from 'express';
-import { placeInPreventive, placeInIncarceration, addSentence } from '../controllers/actions';
+import { 
+  placeInPreventive, placeInIncarceration, addSentence, addFinalDischarge, addSentenceReduction
+} from '../controllers/actions';
 
 const router = express.Router();
 
 router.put('/incarceration/:prisonFileNumber&:criminalCaseNumber', placeInIncarceration);
 router.put('/preventive/:prisonFileNumber&:criminalCaseNumber', placeInPreventive);
 router.put('/sentence/:prisonFileNumber', addSentence);
-router.put('/final_discharge/:prisonFileNumber', addSentence);
-router.put('/sentence_reduction/:prisonFileNumber', addSentence);
+router.put('/final_discharge/:prisonFileNumber', addFinalDischarge);
+router.put('/sentence_reduction/:prisonFileNumber', addSentenceReduction);
 
 export default router;
