1c3226b5831941bc0aa63f85a82f31a11b25ea83
Araken64 <elian.araken@gmail.com>
2020-12-16 19:33:49 +0100

    improve quality

--- src/controllers/actions.ts
+++ src/controllers/actions.ts
@@ -3,6 +3,7 @@ import CriminalCaseModel from '../models/CriminalCase';
 import PrisonerModel, {
   TypeDecision, Sentence, Incarceration, Prevention, FinalDischarge, SentenceReduction,
 } from '../models/Prisoner';
+import datesAreOnSameDay from './util';
 
 interface BodyReqInc {
   decision : {
@@ -40,21 +41,23 @@ export const placeInIncarceration = (req: Request, res: Response) : void => {
     Promise.all([prisonerPromise, criminalCasePromise]).then((values) => {
       if (values.every((value) => value)) {
         const [prisoner, criminalCase] = [values[0]!, values[1]!];
-        if (!prisoner.criminalCase.includes(criminalCase.criminalCaseNumber)) {
-          prisoner.criminalCase.unshift(criminalCase.criminalCaseNumber);
-        }
-        if (!criminalCase.prisoner.includes(prisoner.prisonFileNumber)) {
-          criminalCase.prisoner.push(prisoner.prisonFileNumber);
-        }
-        prisoner.decision.push(reqBody.decision);
-        prisoner.dateOfIncarceration = reqBody.dateOfIncarceration;
-        Promise.all([prisoner.save(), criminalCase.save()])
-          .then(() => res.status(200).json({ message: `Prisoner ${prisoner.prisonFileNumber} is incarcerate for criminal case ${criminalCase.criminalCaseNumber}` }))
-          .catch((error) => res.status(400).json({ error }));
+        if (prisoner.decision.every((decision) => decision.type !== TypeDecision.INC)) {
+          if (!prisoner.criminalCase.includes(criminalCase.criminalCaseNumber)) {
+            prisoner.criminalCase.unshift(criminalCase.criminalCaseNumber);
+          }
+          if (!criminalCase.prisoner.includes(prisoner.prisonFileNumber)) {
+            criminalCase.prisoner.push(prisoner.prisonFileNumber);
+          }
+          prisoner.decision.push(reqBody.decision);
+          prisoner.dateOfIncarceration = reqBody.dateOfIncarceration;
+          Promise.all([prisoner.save(), criminalCase.save()])
+            .then(() => res.status(200).json({ message: `Prisoner ${prisoner.prisonFileNumber} is incarcerate for criminal case ${criminalCase.criminalCaseNumber}` }))
+            .catch((error) => res.status(400).json({ error }));
+        } else res.status(403).json({ error: `Prisoner ${prisoner.prisonFileNumber} is already incarcerate` });
       } else if (!values[0]) res.status(404).json({ error: 'Prisoner not found' });
       else if (!values[1]) res.status(404).json({ error: 'CriminalCase not found' });
     }).catch((error) => res.status(400).json({ error }));
-  } else res.status(403).json({ error: `Request body ${req.body} is not conform to an Incarceration body` });
+  } else res.status(403).json({ error: 'Request body is not conform to an Incarceration body' });
 };
 
 interface BodyReqPre {
@@ -85,7 +88,8 @@ export const placeInPreventive = (req: Request, res: Response) : void => {
     Promise.all([prisonerPromise, criminalCasePromise]).then((values) => {
       if (values.every((value) => value)) {
         const [prisoner, criminalCase] = [values[0]!, values[1]!];
-        if (prisoner.decision.every((decision) => decision.type !== TypeDecision.INC)) {
+        if (prisoner.decision.every((decision) => decision.type !== TypeDecision.INC
+        && decision.type !== TypeDecision.PRE)) {
           if (!prisoner.criminalCase.includes(criminalCase.criminalCaseNumber)) {
             prisoner.criminalCase.unshift(criminalCase.criminalCaseNumber);
           }
@@ -96,11 +100,11 @@ export const placeInPreventive = (req: Request, res: Response) : void => {
           Promise.all([prisoner.save(), criminalCase.save()])
             .then(() => res.status(200).json({ message: `Prisoner ${prisoner.prisonFileNumber} is in preventive for criminal case ${criminalCase.criminalCaseNumber}` }))
             .catch((error) => res.status(400).json({ error }));
-        } else res.status(403).json({ error: `Prisoner ${prisoner.prisonFileNumber} is already incarcerate, he cannot be in preventive` });
+        } else res.status(403).json({ error: `Prisoner ${prisoner.prisonFileNumber} is already incarcerate or in preventive` });
       } else if (!values[0]) res.status(404).json({ error: 'Prisoner not found' });
       else if (!values[1]) res.status(404).json({ error: 'CriminalCase not found' });
     }).catch((error) => res.status(400).json({ error }));
-  } else res.status(403).json({ error: `Request body ${req.body} is not conform to an Preventive body` });
+  } else res.status(403).json({ error: 'Request body is not conform to an Preventive body' });
 };
 
 interface BodyReqSen {
@@ -126,16 +130,18 @@ export const addSentence = (req: Request, res: Response) => {
     PrisonerModel.findOne({ prisonFileNumber: req.params.prisonFileNumber })
       .then((prisoner) => {
         if (prisoner) {
-          prisoner.decision.push(sentence);
-          prisoner.save()
-            .then(() => {
-              const { type, dateOfDecision } = sentence;
-              res.status(200).json({ message: `Decision of ${type} has been taken for Prisoner ${prisoner.prisonFileNumber} the ${dateOfDecision}` });
-            })
-            .catch((error) => res.status(400).json({ error }));
+          const { type, dateOfDecision } = sentence;
+          const noDecAlreadyTaken = prisoner.decision.every((dec) => dec.type !== TypeDecision.SEN
+          || !datesAreOnSameDay(new Date(dec.dateOfDecision), new Date(dateOfDecision)));
+          if (noDecAlreadyTaken) {
+            prisoner.decision.push(sentence);
+            prisoner.save()
+              .then(() => res.status(200).json({ message: `Decision of ${type} has been taken for Prisoner ${prisoner.prisonFileNumber} the ${dateOfDecision}` }))
+              .catch((error) => res.status(400).json({ error }));
+          } else res.status(403).json({ error: `Decision of ${type} has already been taken for Prisoner ${prisoner.prisonFileNumber} the same day` });
         } else res.status(404).json({ error: 'Prisoner not found' });
       }).catch((error) => res.status(400).json({ error }));
-  } else res.status(403).json({ error: `Request body ${req.body} is not conform to a Sentence body` });
+  } else res.status(403).json({ error: 'Request body is not conform to a Sentence body' });
 };
 
 interface BodyReqFin {
@@ -161,16 +167,18 @@ export const addFinalDischarge = (req: Request, res: Response) => {
     PrisonerModel.findOne({ prisonFileNumber: req.params.prisonFileNumber })
       .then((prisoner) => {
         if (prisoner) {
-          prisoner.decision.push(finalDisc);
-          prisoner.save()
-            .then(() => {
-              const { type, dateOfDecision } = finalDisc;
-              res.status(200).json({ message: `Decision of ${type} has been taken for Prisoner ${prisoner.prisonFileNumber} the ${dateOfDecision}` });
-            })
-            .catch((error) => res.status(400).json({ error }));
+          const { type, dateOfDecision } = finalDisc;
+          const noDecAlreadyTaken = prisoner.decision.every((dec) => dec.type !== TypeDecision.FIN
+          || !datesAreOnSameDay(new Date(dec.dateOfDecision), new Date(dateOfDecision)));
+          if (noDecAlreadyTaken) {
+            prisoner.decision.push(finalDisc);
+            prisoner.save()
+              .then(() => res.status(200).json({ message: `Decision of ${type} has been taken for Prisoner ${prisoner.prisonFileNumber} the ${dateOfDecision}` }))
+              .catch((error) => res.status(400).json({ error }));
+          } else res.status(403).json({ error: `Decision of ${type} has already been taken for Prisoner ${prisoner.prisonFileNumber} the same day` });
         } else res.status(404).json({ error: 'Prisoner not found' });
       }).catch((error) => res.status(400).json({ error }));
-  } else res.status(403).json({ error: `Request body ${req.body} is not conform to a Final Discharge body` });
+  } else res.status(403).json({ error: 'Request body is not conform to a Final Discharge body' });
 };
 
 interface BodyReqRed {
@@ -196,14 +204,16 @@ export const addSentenceReduction = (req: Request, res: Response) => {
     PrisonerModel.findOne({ prisonFileNumber: req.params.prisonFileNumber })
       .then((prisoner) => {
         if (prisoner) {
-          prisoner.decision.push(sentReduc);
-          prisoner.save()
-            .then(() => {
-              const { type, dateOfDecision } = sentReduc;
-              res.status(200).json({ message: `Decision of ${type} has been taken for Prisoner ${prisoner.prisonFileNumber} the ${dateOfDecision}` });
-            })
-            .catch((error) => res.status(400).json({ error }));
+          const { type, dateOfDecision } = sentReduc;
+          const noDecAlreadyTaken = prisoner.decision.every((dec) => dec.type !== TypeDecision.RED
+          || !datesAreOnSameDay(new Date(dec.dateOfDecision), new Date(dateOfDecision)));
+          if (noDecAlreadyTaken) {
+            prisoner.decision.push(sentReduc);
+            prisoner.save()
+              .then(() => res.status(200).json({ message: `Decision of ${type} has been taken for Prisoner ${prisoner.prisonFileNumber} the ${dateOfDecision}` }))
+              .catch((error) => res.status(400).json({ error }));
+          } else res.status(403).json({ error: `Decision of ${type} has already been taken for Prisoner ${prisoner.prisonFileNumber} the same day` });
         } else res.status(404).json({ error: 'Prisoner not found' });
       }).catch((error) => res.status(400).json({ error }));
-  } else res.status(403).json({ error: `Request body ${req.body} is not conform to a Sentence Reduction body` });
+  } else res.status(403).json({ error: 'Request body is not conform to a Sentence Reduction body' });
 };
