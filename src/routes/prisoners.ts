df6777656b025876ff03e1c1cc0febb24cfaa328
Araken64 <elian.araken@gmail.com>
2020-12-16 19:36:36 +0100

    add getOne route for prisoner

--- src/routes/prisoners.ts
+++ src/routes/prisoners.ts
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post('/', prisonersCtrl.createPrisoner);
 router.get('/', prisonersCtrl.getAllPrisoners);
+router.get('/:prisonFileNumber', prisonersCtrl.getOnePrisoner);
 router.put('/:prisonFileNumber', prisonersCtrl.updatePrisoner);
 router.delete('/:prisonFileNumber', prisonersCtrl.deletePrisoner);
 
